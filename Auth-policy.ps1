fed23fd6f472e689c322e2efdf7e44920ef5d5b8
shnl <53551518+shnlnryn@users.noreply.github.com>
2019-12-13 15:33:12 +1100

    Update and rename Auth-policy to Auth-policy.ps1

--- Auth-policy.ps1
+++ Auth-policy.ps1
@@ -1,3 +1,10 @@
+#Set authentication policy for all users in a domain.
+
+$cred = Get-Credential
+
+Connect-EXOPSSession -Credential $cred
+
+
 $Users= get-user -ResultSize unlimited | where {$_.userprincipalname -like "*@mydomain.com" -and $_.authenticationpolicy -eq $null}|Select-Object userprincipalname, authenticationpolicy
 
 foreach ($User in $Users){
